a59077ea4f912ca10fc773e1b845bc1e322954d2
Thet Htoo Aung <thethtooaung@Thets-MacBook-Air.local>
2026-05-14 16:55:51 +0630

    ci: prevent deleting a category with related data:  cost and collection, showing proper message

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['user_id', 'name', 'color', 'is_active'])]
 class Category extends Model
@@ -11,5 +13,15 @@ class Category extends Model
     public function user() 
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function collections(): BelongsToMany
+    {
+        return $this->belongsToMany(Collection::class, 'category_collection');
+    }
+
+    public function costs(): HasMany
+    {
+        return $this->hasMany(Cost::class);
     }
 }
--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,8 +2,8 @@
 
 namespace App\Services;
 
-use App\Repositories\BaseRepository;
 use App\Repositories\CategoryRepository;
+use Override;
 
 class CategoryService extends BaseService
 {
@@ -25,5 +25,23 @@ class CategoryService extends BaseService
     public function getMine() 
     {
         return $this->repository->getMyCategories();
+    }
+
+    #[Override]
+    public function delete(int|string $id): bool
+    {
+        $category = $this->repository->find($id);
+
+        // Check if the category is used in the cost
+        if ($category->costs()->exists()) {
+            throw new \Exception('The category is used in a cost!');
+        }
+
+        // Check if the category is used in the collection
+        if($category->collections()->exists()) {
+            throw new \Exception('The category is used in a collection!');
+        }
+        
+        return parent::delete($id);
     }
 }
